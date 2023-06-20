d623923ca4578711e09a4033b7628a6f9361f41f
Lars Hoeijmans <474593@student.fontys.nl>
2023-06-20 21:39:01 +0200

    RSS feed works, but....
    RSS feed works, but is not scrollable and does not include images.

--- src/components/EredivisieNews.vue
+++ src/components/EredivisieNews.vue
@@ -1,49 +1,41 @@
 <template>
-  <div class="news-feed">
-    <div v-if="isLoading" class="loading-message">Loading...</div>
-    <div v-else-if="newsItems.length === 0" class="no-news-message">No news available.</div>
-    <div v-else class="news-items">
-      <div v-for="item in newsItems" :key="item.link" class="news-item">
-        <h2>{{ item.title }}</h2>
-        <p>{{ item.contentSnippet }}</p>
-        <a :href="item.link" target="_blank">Read more</a>
-      </div>
-    </div>
+  <div>
+    <h2>{{ title }}</h2>
+    <ul>
+      <li v-for="item in items" :key="item.guid">
+        <a :href="item.link" target="_blank">{{ item.title }}</a>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
+import axios from 'axios';
 import Parser from 'rss-parser';
 
 export default {
   data() {
     return {
-      newsItems: [], // Holds the fetched news data
-      isLoading: true, // Indicates if the data is currently being fetched
+      title: '',
+      items: []
     };
   },
   mounted() {
-    // Fetch the Eredivisie news data from the RSS feed
-    this.fetchNewsData();
+    this.fetchRssFeed();
   },
   methods: {
-    async fetchNewsData() {
-      const parser = new Parser();
+    async fetchRssFeed() {
       try {
-        const feed = await parser.parseURL('https://www.espn.com/espn/rss/soccer/news'); // Place the RSS feed URL
-        this.newsItems = feed.items;
+        const response = await axios.get('https://www.espn.com/espn/rss/soccer/news');
+        const parser = new Parser();
+        const feed = await parser.parseString(response.data);
+
+        this.title = feed.title;
+        this.items = feed.items;
       } catch (error) {
         console.error('Error fetching RSS feed:', error);
-      } finally {
-        this.isLoading = false;
       }
-    },
-  },
+    }
+  }
 };
 </script>
-
-<style scoped>
-.no-news-message {
-  color: black;
-}
-</style>
